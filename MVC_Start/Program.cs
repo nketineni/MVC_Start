22e8679a52d10b568d6846a9e88ee308a952644e
Neelima Ketineni <n_Ketineni@yahoo.com>
2020-06-21 18:44:31 -0400

    Update by Neelima

--- MVC_Start/Program.cs
+++ MVC_Start/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ namespace MVC_Start
     public static void Main(string[] args)
     {
       BuildWebHost(args).Run();
+            Debug.WriteLine("update by Neelima");
     }
 
     public static IWebHost BuildWebHost(string[] args) =>
